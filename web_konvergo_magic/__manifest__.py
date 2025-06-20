e650ea368fc3d1ee1926dc2d4c64bec3d35bf31b
Lanhild <loanj@naver.com>
2025-06-20 15:25:00 -0400

    Bump version

--- web_konvergo_magic/__manifest__.py
+++ web_konvergo_magic/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Konvergo MAGIC",
     "summary": "Konvergo MAGIC baseline for integration with Konvergo ERP",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Konvergo",
     "maintainer": "Konvergo",
     "website": "https://konvergo.com",
